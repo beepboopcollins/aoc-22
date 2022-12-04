039e09d11ac26a445b8c0afd1f837dfc11cd8a79
George Collins <george.collins@waterstons.com>
2022-12-04 11:51:06 +0000

    day 4 not very elegant sorry.

---
+++ 04/main.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Elf:
+    start_zone_id: int
+    end_zone_id: int
+
+
+@dataclass
+class ElfPair:
+    elf1: Elf
+    elf2: Elf
+
+    def one_contains_two(self) -> bool:
+        return (self.elf1.start_zone_id <= self.elf2.start_zone_id) and (self.elf1.end_zone_id >= self.elf2.end_zone_id)
+
+    def two_contains_one(self) -> bool:
+        return (self.elf2.start_zone_id <= self.elf1.start_zone_id) and (self.elf2.end_zone_id >= self.elf1.end_zone_id)
+
+    def one_contains_other(self) -> bool:
+        return self.one_contains_two() or self.two_contains_one()
+
+    def one_ends_in_two(self) -> bool:
+        return (self.elf2.start_zone_id <= self.elf1.end_zone_id <= self.elf2.end_zone_id)
+
+    def two_ends_in_one(self)-> bool:
+        return (self.elf1.start_zone_id <= self.elf2.end_zone_id <= self.elf1.end_zone_id)
+
+    def has_overlap(self) -> bool:
+        return self.one_ends_in_two() or self.two_ends_in_one()
+
+def read_file(filename: str) -> str:
+    with open(filename, "r") as f:
+        raw = f.read()
+    return raw
+
+
+def write_file(filename: str, data: str):
+    with open(filename, "w") as f:
+        f.write(data)
+
+
+def transform_raw_in(raw_in: list[str]) -> list[list[tuple[int, int]]]:
+    raw_pairs = []
+    for line in raw_in:
+        this_pair = []
+        elves = line.split(",")
+        for elf in elves:
+            start_str, end_str = elf.split("-", 2)
+            this_pair.append((int(start_str), int(end_str)))
+        raw_pairs.append(this_pair)
+    return raw_pairs
+
+
+def get_pairs(raw_pairs: list[list[tuple[int, int]]]) -> list[ElfPair]:
+    pairs = []
+    for raw_pair in raw_pairs:
+        elf1_raw, elf2_raw = raw_pair
+        pairs.append(ElfPair(Elf(*elf1_raw), Elf(*elf2_raw)))
+    return pairs
+
+
+def main():
+    raw_in = read_file("in.txt").split("\n")
+    raw_pairs = transform_raw_in(raw_in)
+    pairs = get_pairs(raw_pairs)
+    total_fully_contained = sum(pair.one_contains_other() for pair in pairs)
+    write_file("pt1.txt", str(total_fully_contained))
+    total_overlap = sum(pair.has_overlap() for pair in pairs)
+    write_file("pt2.txt", str(total_overlap))
+
+
+if __name__ == "__main__":
+    main()
