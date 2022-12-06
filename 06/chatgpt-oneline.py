d977d56b57bc365f47f42e8bba8b1769bad7ebcc
George Collins <george.collins@waterstons.com>
2022-12-06 10:41:03 +0000

    day 6 chatgpt oneline

---
+++ 06/chatgpt-oneline.py
@@ -0,0 +1,2 @@
+print(open("in.txt").read().index(next((open("in.txt").read()[i:i+4] for i in range(len(open("in.txt").read())) if len(set(open("in.txt").read()[i:i+4])) == 4), ""))+4)
+print(open("in.txt").read().index(next((open("in.txt").read()[i:i+14] for i in range(len(open("in.txt").read())) if len(set(open("in.txt").read()[i:i+14])) == 14), ""))+14)
